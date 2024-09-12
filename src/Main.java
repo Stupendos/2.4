bd5aea0614bff459a99735911e9b4567366073d6
Stupendos <topakovdenis671@gmail.com>
2024-09-12 21:36:22 +0500

    CheckPls

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        inputDataUser("kokojambo", "1234qwER", "1234qwER");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+
+    public static void inputDataUser(String login, String password, String confirmPassword) {
+        String regexLogin = "^[a-z0-9_]{0,20}$";
+        Pattern patternLogin = Pattern.compile(regexLogin);
+        Matcher matcherLogin = patternLogin.matcher(login);
+
+
+        String regexPassword = "^[a-zA-Z0-9_]{0,20}$";
+        Pattern patternPassword = Pattern.compile(regexPassword);
+        Matcher matcherPassword = patternPassword.matcher(password);
+
+
+        try {
+            if (!matcherLogin.matches()) {
+                throw new WrongLoginException();
+            }if(!matcherPassword.matches()) {
+                throw new WrongPasswordException();
+            }
+            if(!password.equals(confirmPassword)) {
+                throw new WrongPasswordException();
+            }
+
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Ошибка " + e.getMessage());
         }
     }
 }
